2a63a236e21852059fedd936da3ce469901c3074
camsh69 <camsh69@gmail.com>
2022-02-22 20:01:08 +0000

    added File annotations

--- src/main/java/com/day2Homework/example/day2Homework/models/File.java
+++ src/main/java/com/day2Homework/example/day2Homework/models/File.java
@@ -1,11 +1,30 @@
 package com.day2Homework.example.day2Homework.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="files")
 public class File {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="extension")
     private String extension;
+
+    @Column(name="size")
     private int size;
+
+    @JsonIgnoreProperties({"files"})
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
     private Folder folder;
 
     public File(String name, String extension, int size, Folder folder) {
