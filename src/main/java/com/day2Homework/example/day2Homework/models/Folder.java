972eaae8708b642568f666cf0943c88d41e78474
camsh69 <camsh69@gmail.com>
2022-02-22 21:35:49 +0000

    bug fixes

--- src/main/java/com/day2Homework/example/day2Homework/models/Folder.java
+++ src/main/java/com/day2Homework/example/day2Homework/models/Folder.java
@@ -3,7 +3,6 @@ package com.day2Homework.example.day2Homework.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
